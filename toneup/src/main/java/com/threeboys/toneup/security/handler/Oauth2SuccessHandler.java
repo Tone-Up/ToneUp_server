6161446300b7692971593181e63cab80a18a6292
김승용 <gkstjeo17@naver.com>
2025-05-29 00:08:32 +0900

    refactor: 웹 로그인 시 Oauth2SuccessHandler dto 빌더 형식으로 변환

--- toneup/src/main/java/com/threeboys/toneup/security/handler/Oauth2SuccessHandler.java
+++ toneup/src/main/java/com/threeboys/toneup/security/handler/Oauth2SuccessHandler.java
@@ -5,10 +5,12 @@ import com.threeboys.toneup.common.response.StandardResponse;
 import com.threeboys.toneup.security.CustomOAuth2User;
 import com.threeboys.toneup.security.jwt.JWTUtil;
 import com.threeboys.toneup.security.response.OAuthLoginResponseDTO;
+import com.threeboys.toneup.user.entity.UserEntity;
+import com.threeboys.toneup.user.repository.UserRepository;
+import com.threeboys.toneup.user.service.Userservice;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -23,9 +25,13 @@ import java.util.Iterator;
 @Component
 public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
     private final JWTUtil jwtUtil;
+    private final Userservice userservice;
+    private final UserRepository userRepository;
 
-    public Oauth2SuccessHandler(JWTUtil jwtUtil) {
+    public Oauth2SuccessHandler(JWTUtil jwtUtil, Userservice userservice, UserRepository userRepository) {
         this.jwtUtil = jwtUtil;
+        this.userservice = userservice;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -43,15 +49,16 @@ public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
 
         String accessToken = jwtUtil.createJwt(userId,nickname, personalColor, role, 60*60*60L);
         String refreshToken = jwtUtil.createRefreshJwt(userId, 60*60*24*14L);
-
-        OAuthLoginResponseDTO data = new OAuthLoginResponseDTO();
-        data.setProvider(privider);
-        data.setPersonal(false); // 필요에 따라 변경
-        data.setSignedUp(true);  // 필요에 따라 변경
-        data.setNickname(nickname);
-        data.setUserId(userId);
-        data.setAccessToken(accessToken);
-        data.setRefreshToken(refreshToken);
+        UserEntity socialUser = userRepository.findById(userId).orElseThrow();
+        OAuthLoginResponseDTO data = OAuthLoginResponseDTO.builder()
+                .provider(privider)
+                .nickname(nickname)
+                .userId(userId)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .isSignedUp(userservice.isSignedUp(socialUser))
+                .isPersonal(userservice.isPersonal(socialUser))
+                .build();
 
         StandardResponse<OAuthLoginResponseDTO> responseBody = new StandardResponse<>(true, 0, "Ok", data);
 
