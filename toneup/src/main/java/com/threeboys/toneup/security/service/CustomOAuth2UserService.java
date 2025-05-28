64af8f72079e15e3837f6ea3a7697533826be3f6
김승용 <gkstjeo17@naver.com>
2025-05-29 00:00:07 +0900

    style: sout 제거

--- toneup/src/main/java/com/threeboys/toneup/security/service/CustomOAuth2UserService.java
+++ toneup/src/main/java/com/threeboys/toneup/security/service/CustomOAuth2UserService.java
@@ -9,6 +9,7 @@ import com.threeboys.toneup.user.domain.User;
 import com.threeboys.toneup.user.dto.UserDTO;
 import com.threeboys.toneup.user.entity.UserEntity;
 import com.threeboys.toneup.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -16,7 +17,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Slf4j
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
@@ -29,9 +30,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        System.out.println(oAuth2User);
-
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         OAuth2Response oAuth2Response = null;
         if (registrationId.equals("naver")) {
