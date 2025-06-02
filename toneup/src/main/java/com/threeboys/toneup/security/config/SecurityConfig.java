39f89fb12b5c1a3699d0514f737ab83589afe38b
kimsy98 <gkstjeo17@naver.com>
2025-06-02 13:03:46 +0900

    init: SecurityConfig app 소셜 로그인 url 경로 통과 설정

--- toneup/src/main/java/com/threeboys/toneup/security/config/SecurityConfig.java
+++ toneup/src/main/java/com/threeboys/toneup/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -72,5 +73,10 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/api/app/authorization");
     }
 }
