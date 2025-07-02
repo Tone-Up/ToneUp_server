4aa0828ede6361df3f99591999b72755579efe98
kimsy98 <gkstjeo17@naver.com>
2025-07-02 16:47:23 +0900

    fix: SecurityConfig 경로별 인가 설정 수정

--- toneup/src/main/java/com/threeboys/toneup/security/config/SecurityConfig.java
+++ toneup/src/main/java/com/threeboys/toneup/security/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/").permitAll()
                         .requestMatchers("/api/app/**").authenticated()
                         .anyRequest().authenticated());
 //                        .anyRequest().denyAll();
