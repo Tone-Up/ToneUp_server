87a3bf2d3c415369e22742ae3b4c73a519bdad0b
kimsy98 <gkstjeo17@naver.com>
2025-07-04 20:05:25 +0900

    fix: access, refreshToken expiredMs *1000

--- toneup/src/main/java/com/threeboys/toneup/security/jwt/JWTUtil.java
+++ toneup/src/main/java/com/threeboys/toneup/security/jwt/JWTUtil.java
@@ -57,7 +57,7 @@ public class JWTUtil {
                 .claim("personalColor", personalColor)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs*1000))
                 .signWith(secretKey)
                 .compact();
     }
@@ -66,7 +66,7 @@ public class JWTUtil {
         String refreshToken =  Jwts.builder()
                 .subject(String.valueOf(userId))
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs*1000))
                 .signWith(secretKey)
                 .compact();
         RefreshToken token = new RefreshToken(userId, refreshToken, expiredMs / 1000);
