8a1cf0ebce12b25ef4a7a21ca0388b6620f4ba40
kimsy98 <gkstjeo17@naver.com>
2025-07-05 20:36:02 +0900

    feature : 기본 ProfileImage 저장, profile 수정 api 추가(Dto, 서비스, 컨트롤러 구현)

--- toneup/src/main/java/com/threeboys/toneup/user/controller/UserController.java
+++ toneup/src/main/java/com/threeboys/toneup/user/controller/UserController.java
@@ -3,13 +3,12 @@ package com.threeboys.toneup.user.controller;
 import com.threeboys.toneup.common.response.StandardResponse;
 import com.threeboys.toneup.security.CustomOAuth2User;
 import com.threeboys.toneup.user.dto.ProfileResponse;
+import com.threeboys.toneup.user.dto.UpdateProfileRequest;
 import com.threeboys.toneup.user.service.Userservice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/profiles")
@@ -19,9 +18,21 @@ public class UserController {
     private final Userservice userservice;
 
     @GetMapping("/me")
-    public ResponseEntity<?> getProfile(@AuthenticationPrincipal CustomOAuth2User customOAuth2User){
+    public ResponseEntity<?> getMyProfile(@AuthenticationPrincipal CustomOAuth2User customOAuth2User){
         Long userId = customOAuth2User.getId();
         ProfileResponse profileResponse = userservice.getProfile(userId);
         return ResponseEntity.ok(new StandardResponse<>(true, 0, "Ok", profileResponse));
+    }
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserProfile(@PathVariable Long userId){
+        ProfileResponse profileResponse = userservice.getProfile(userId);
+        return ResponseEntity.ok(new StandardResponse<>(true, 0, "Ok", profileResponse));
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> changeProfile(@RequestBody UpdateProfileRequest updateProfileRequest, @AuthenticationPrincipal CustomOAuth2User customOAuth2User){
+        Long userId = customOAuth2User.getId();
+        userservice.updateProfile(userId, updateProfileRequest);
+        return ResponseEntity.ok(new StandardResponse<>(true, 0, "Ok", null));
     }
 }
