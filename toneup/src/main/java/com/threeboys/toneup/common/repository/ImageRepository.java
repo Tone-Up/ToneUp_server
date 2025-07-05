eb77d85e8487e75412be712f38977b9d21d77c86
kimsy98 <gkstjeo17@naver.com>
2025-07-05 20:33:53 +0900

    fix : Images 엔티티 기본 생성자 에러 해결

--- toneup/src/main/java/com/threeboys/toneup/common/repository/ImageRepository.java
+++ toneup/src/main/java/com/threeboys/toneup/common/repository/ImageRepository.java
@@ -1,5 +1,6 @@
-package com.threeboys.toneup.common.domain;
+package com.threeboys.toneup.common.repository;
 
+import com.threeboys.toneup.common.domain.Images;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
