3b18e06784b1604d19271a501c7b37755e52c608
kimsy98 <gkstjeo17@naver.com>
2025-07-05 20:36:38 +0900

    test : UserServiceTest 프로필 수정 메서드 테스트

--- toneup/src/test/java/com/threeboys/toneup/user/service/UserServiceTest.java
+++ toneup/src/test/java/com/threeboys/toneup/user/service/UserServiceTest.java
@@ -4,7 +4,9 @@ import com.threeboys.toneup.feed.domain.Feed;
 import com.threeboys.toneup.personalColor.domain.PersonalColor;
 import com.threeboys.toneup.personalColor.domain.PersonalColorType;
 import com.threeboys.toneup.security.provider.ProviderType;
+import com.threeboys.toneup.user.domain.User;
 import com.threeboys.toneup.user.dto.ProfileResponse;
+import com.threeboys.toneup.user.dto.UpdateProfileRequest;
 import com.threeboys.toneup.user.entity.UserEntity;
 import com.threeboys.toneup.user.exception.UserNotFoundException;
 import com.threeboys.toneup.user.repository.UserRepository;
@@ -22,7 +24,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -47,5 +49,36 @@ public class UserServiceTest {
         ProfileResponse profileResponse = userservice.getProfile(userId);
 
         assertEquals(expectProfileResponse, profileResponse);
+    }
+    @Test
+    @DisplayName("프로필 수정")
+    void updateProfile(){
+
+        UpdateProfileRequest updateProfileRequest = UpdateProfileRequest.builder()
+                .bio("updateBio")
+                .build();
+
+
+        UserEntity mockEntity = mock(UserEntity.class);
+        User mockDomain = mock(User.class);
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(mockEntity));
+        when(mockEntity.toDomain()).thenReturn(mockDomain);
+
+        // when
+        userservice.updateProfile(1L, updateProfileRequest);
+
+        // then
+        verify(mockDomain).changeBio("updateBio");
+        verify(mockEntity).changeProfile(mockDomain);
+    }
+
+    @Test
+    @DisplayName("프로필 동등성 비교")
+    void equalsProfile(){
+        UserEntity user1 = new UserEntity("김준영", "Jjun", ProviderType.GOOGLE, "test1234", "test1234@test.com");
+        UserEntity user2 = new UserEntity("김준영", "Jjun", ProviderType.GOOGLE, "test1234", "test1234@test.com");
+
+        assertEquals(user1,user2);
     }
 }
