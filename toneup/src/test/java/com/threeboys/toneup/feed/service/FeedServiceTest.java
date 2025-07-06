eb59b07e137520a4f7b5826522f09d3504f7d98e
kimsy98 <gkstjeo17@naver.com>
2025-07-06 14:59:59 +0900

    test : 피드 수정 service 테스트

--- toneup/src/test/java/com/threeboys/toneup/feed/service/FeedServiceTest.java
+++ toneup/src/test/java/com/threeboys/toneup/feed/service/FeedServiceTest.java
@@ -1,23 +1,30 @@
 package com.threeboys.toneup.feed.service;
 
+import com.threeboys.toneup.common.domain.ImageType;
+import com.threeboys.toneup.common.domain.Images;
 import com.threeboys.toneup.common.repository.ImageRepository;
 import com.threeboys.toneup.feed.domain.Feed;
 import com.threeboys.toneup.feed.dto.FeedRequest;
 import com.threeboys.toneup.feed.dto.FeedResponse;
 import com.threeboys.toneup.feed.repository.FeedRepository;
+import com.threeboys.toneup.security.provider.ProviderType;
 import com.threeboys.toneup.user.entity.UserEntity;
 import com.threeboys.toneup.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -56,6 +63,32 @@ class FeedServiceTest {
         FeedResponse response = feedService.createFeed(userId, feedRequest);
 
         // then
+        assertThat(response.getFeedId()).isEqualTo(100L);
+    }
+    @Test
+    @DisplayName("피드 수정하기")
+    void changeFeed(){
+        // given
+        Long feedId = 100L;
+        Long userId = 1L;
+        FeedRequest request = new FeedRequest("새로운 내용", List.of("url1", "url2"));
+        UserEntity user = new UserEntity(userId);
+
+        Feed feed = Mockito.spy(new Feed(user,"기존 내용")); // 변경 전 피드
+
+        given(feedRepository.findById(feedId)).willReturn(Optional.of(feed));
+
+        // when
+        FeedResponse response = feedService.updateFeed(userId, feedId, request);
+
+        // then
+        // feed 내용 변경 확인
+        verify(feed).changeFeed("새로운 내용", List.of("url1", "url2"));
+
+        // 이미지 삭제 및 저장 확인
+        verify(imageRepository).deleteByTypeAndRefId(ImageType.FEED, feedId);
+        verify(imageRepository).saveAll(feed.getImageUrlList());
+
         assertThat(response.getFeedId()).isEqualTo(100L);
     }
 }
