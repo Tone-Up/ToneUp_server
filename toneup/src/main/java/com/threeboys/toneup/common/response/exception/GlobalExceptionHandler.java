bcbf4c4634330a28eb11a4620b7691ffdad12ab3
kimsy98 <gkstjeo17@naver.com>
2025-07-07 19:35:15 +0900

    feature: S3_DELETE_FAILED 예외 구현

--- toneup/src/main/java/com/threeboys/toneup/common/response/exception/GlobalExceptionHandler.java
+++ toneup/src/main/java/com/threeboys/toneup/common/response/exception/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.threeboys.toneup.common.response.exception;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.threeboys.toneup.common.response.ErrorResponse;
 import com.threeboys.toneup.common.response.StandardResponse;
 import com.threeboys.toneup.security.exception.InvalidRefreshTokenException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidTokenException.class)
@@ -22,5 +26,15 @@ public class GlobalExceptionHandler {
                 false, 401, ex.getMessage(), null
         );
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
+    }
+
+    @ExceptionHandler({SdkClientException.class, AmazonServiceException.class})
+    public ResponseEntity<ErrorResponse<Object>> handleS3Exception(Exception ex) {
+        log.error(ex.getMessage());
+        ErrorResponse<Object> body = new ErrorResponse<>(
+                //추후 ErrorCode(enum 타입)으로 관리 필요
+                500, "S3_DELETE_FAILED", ErrorMessages.S3_DELETE_FAILED
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
     }
 }
