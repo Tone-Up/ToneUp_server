3e28983e19fe3d809e3de2bd5f7b3cc1dafb59f0
kimsy98 <gkstjeo17@naver.com>
2025-07-23 21:43:57 +0900

    feature: 네이티브 쿼리 사용시 id 값으로 조회 위해 code 추가

--- toneup/src/main/java/com/threeboys/toneup/personalColor/domain/PersonalColorType.java
+++ toneup/src/main/java/com/threeboys/toneup/personalColor/domain/PersonalColorType.java
@@ -1,5 +1,29 @@
 package com.threeboys.toneup.personalColor.domain;
 
 public enum PersonalColorType {
-    SPRING, SUMMER, AUTUMN, WINTER
+    SPRING(1),
+    SUMMER(2),
+    AUTUMN(3),
+    WINTER(4);
+
+    private final int code;
+
+    PersonalColorType(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    // 역으로 숫자 → enum
+    public static PersonalColorType fromCode(int code) {
+        for (PersonalColorType type : values()) {
+            if (type.code == code) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid code: " + code);
+    }
 }
+
