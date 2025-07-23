6adfec9dc1fa947b76d4ecb0106522021e1b566e
kimsy98 <gkstjeo17@naver.com>
2025-07-23 21:44:52 +0900

    feature: 추천 상품 페이지네이션 위한 컨트롤러, 서비스, dto, 레디스 캐싱 처리 구현

---
+++ toneup/src/main/java/com/threeboys/toneup/product/repository/CustomProductRepository.java
@@ -0,0 +1,9 @@
+package com.threeboys.toneup.product.repository;
+
+import com.threeboys.toneup.recommand.dto.ProductPageItemResponse;
+
+import java.util.List;
+
+public interface CustomProductRepository {
+    ProductPageItemResponse findProductWithImageAndIsLiked(Long userId, Long cursor,  int limit, List<Long> randomIdList);
+}
---
+++ toneup/src/main/java/com/threeboys/toneup/recommand/repository/ProductPersonalColorRepository.java
@@ -0,0 +1,25 @@
+package com.threeboys.toneup.recommand.repository;
+
+import com.threeboys.toneup.personalColor.domain.PersonalColorType;
+import com.threeboys.toneup.recommand.domain.ProductPersonalColor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductPersonalColorRepository extends JpaRepository<ProductPersonalColor, Long> {
+
+    @Query(value = "select product.id from productpersonalcolor " +
+            "join product on  productpersonalcolor.product_id=product.id " +
+            "where productpersonalcolor.personalColor_id=:personalColorId " +
+            "order by rand(:seed) limit :limit offset :offset", nativeQuery = true)
+    List<Long> findByRandomProductIdList(@Param("personalColorId")int personalColorId, @Param("limit")int limit, @Param("offset")Long offset, @Param("seed") Long seed);
+
+
+    @Query(value = "select product.id from productpersonalcolor join product on  productpersonalcolor.product_id=product.id where productpersonalcolor.personalColor_id=:personalColorId limit :limit offset :offset", nativeQuery = true)
+    List<Long> findByPersonalColorIdAndUserId(@Param("personalColorId")int personalColorId, @Param("limit")int limit, @Param("offset")Long offset);
+
+}
