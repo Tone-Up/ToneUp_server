eed07b1b5b2ca1a709b3ba432a2b21d1f0c88493
kimsy98 <gkstjeo17@naver.com>
2025-08-23 19:00:52 +0900

    feature: 벡터 스토어 모델 설정 및 jedis 빈 생성

--- toneup/src/main/java/com/threeboys/toneup/common/config/RedisSearchConfig.java
+++ toneup/src/main/java/com/threeboys/toneup/common/config/RedisSearchConfig.java
@@ -3,10 +3,19 @@ package com.threeboys.toneup.common.config;
 import com.redislabs.lettusearch.RediSearchClient;
 import com.redislabs.lettusearch.StatefulRediSearchConnection;
 import io.lettuce.core.RedisURI;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.*;
 
 @Configuration
 public class RedisSearchConfig {
@@ -27,6 +36,9 @@ public class RedisSearchConfig {
     @Value("${spring.data.redis.password}")
     private String redisPassword;
 
+    @Value(("${spring.ai.openai.api-key}"))
+    private String openApiKey;
+
     @Bean
     public RediSearchClient rediSearchClient() {
         RedisURI redisUri = RedisURI.Builder
@@ -41,4 +53,28 @@ public class RedisSearchConfig {
     public StatefulRediSearchConnection<String, String> searchConnection(RediSearchClient rediSearchClient) {
         return rediSearchClient.connect();
     }
+
+    @Bean
+    public JedisPooled jedisPooled() {
+        DefaultJedisClientConfig config = DefaultJedisClientConfig.builder()
+                .password(redisPassword)   // Redis 비밀번호
+                .build();
+        return new JedisPooled(new HostAndPort(masterHost, masterPort), config);
+    }
+
+    @Bean
+    public VectorStore vectorStore(JedisPooled jedisPooled, EmbeddingModel embeddingModel) {
+
+        return  RedisVectorStore.builder(jedisPooled, embeddingModel)
+                .initializeSchema(true) // ← 여기 꼭 true
+                .build();
+    }
+
+//    @Bean
+//    public EmbeddingModel embeddingModel() {
+//
+//        return new OpenAiEmbeddingModel(new OpenAiApi(openApiKey));
+//    }
+
+
 }
