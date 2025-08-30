ba88ba5026a9db2715b5dfea432eaa163340547a
kimsy98 <gkstjeo17@naver.com>
2025-08-30 17:02:05 +0900

    feature: BotMessageType 에 name 값 추가

--- toneup/src/main/java/com/threeboys/toneup/chatbot/dto/BotMessageType.java
+++ toneup/src/main/java/com/threeboys/toneup/chatbot/dto/BotMessageType.java
@@ -1,5 +1,14 @@
 package com.threeboys.toneup.chatbot.dto;
 
+import lombok.Getter;
+
+@Getter
 public enum BotMessageType {
-    READY, QnA, RECOMMEND_CODI, EVALUATE_CODI
+    READY("계속 질문하기"), QnA("Q&A"), RECOMMEND_CODI("AI 상품 추천"), EVALUATE_CODI("코디 평가");
+
+    private final String typeName;
+
+    BotMessageType(String typeName) {
+            this.typeName = typeName;
+    }
 }
