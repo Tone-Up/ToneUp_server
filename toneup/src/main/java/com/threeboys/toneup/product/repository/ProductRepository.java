78d6007405806c09e6d36379d5beb0988c3a839a
kimsy98 <gkstjeo17@naver.com>
2026-01-28 18:27:40 +0900

    test: 레디스서치 성능 비교를 위해 자동완성 기능 db 조회 쿼리 생성

--- toneup/src/main/java/com/threeboys/toneup/product/repository/ProductRepository.java
+++ toneup/src/main/java/com/threeboys/toneup/product/repository/ProductRepository.java
@@ -2,9 +2,19 @@ package com.threeboys.toneup.product.repository;
 
 import com.threeboys.toneup.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, CustomProductRepository {
 
+    @Query("SELECT p.productName " +
+            "FROM Product p " +
+            "WHERE p.productName LIKE %:kw% OR p.brand LIKE %:kw% " +
+            "GROUP BY p.productName " +
+            "ORDER BY SUM(CASE WHEN p.brand LIKE %:kw% THEN 5 ELSE 1 END) DESC")
+    List<String> searchByKeyword(@Param("kw") String keyword);
 }
